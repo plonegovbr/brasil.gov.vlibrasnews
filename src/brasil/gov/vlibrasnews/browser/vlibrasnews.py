7cae9ea77f99bc5022be6d4762995f979ced80ca
hvelarde <hector.velarde@gmail.com>
2016-08-01 16:41:43 -0300

    Refactor to implement caching

--- src/brasil/gov/vlibrasnews/browser/vlibrasnews.py
+++ src/brasil/gov/vlibrasnews/browser/vlibrasnews.py
@@ -5,29 +5,40 @@ from brasil.gov.vlibrasnews.interfaces import IVLibrasNewsSettings
 from brasil.gov.vlibrasnews.subscribers import get_video_url
 from plone import api
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.memoize import ram
+from time import time
 
 
 class VLibrasNewsViewlet(ViewletBase):
 
     """Viewlet with vlibrasnews button."""
 
-    youtube_url = None
-    is_ready = False
-    enabled = False
-    state = 'processing'
+    @property
+    def enabled(self):
+        record = IVLibrasNewsSettings.__identifier__ + '.enabled_content_types'
+        enabled_content_types = api.portal.get_registry_record(record)
+        if self.context.portal_type not in enabled_content_types:
+            return False
 
-    def update(self):
-        super(VLibrasNewsViewlet, self).update()
+        is_ready = self.state == 'ready'
+        return is_ready or not api.user.is_anonymous()
+
+    @property
+    def state(self):
         try:
-            self.youtube_url = get_video_url(self.context)
-            self.is_ready = self.youtube_url is not None
-            if self.is_ready:
-                self.state = 'ready'
+            video_url = self.video_url
         except NotProcessingError:
-            self.state = 'notprocessing'
-        finally:
-            record = '{0}.enabled_content_types'.format(
-                IVLibrasNewsSettings.__identifier__)
-            enabled_content_types = api.portal.get_registry_record(record)
-            if self.context.portal_type in enabled_content_types:
-                self.enabled = self.is_ready or not api.user.is_anonymous()
+            return 'notprocessing'
+
+        if video_url is None:
+            return 'processing'
+
+        return 'ready'
+
+    @ram.cache(lambda method, self, context: (time() // 60, context))
+    def _get_video_url(self, context):
+        return get_video_url(context)
+
+    @property
+    def video_url(self):
+        return self._get_video_url(self.context)
