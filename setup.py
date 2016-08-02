6a2fc2876d87c30ac90b68c0ca57564d26f6965f
hvelarde <hector.velarde@gmail.com>
2016-08-01 21:20:11 -0300

    Back to development: 1.0b1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0a1'
+version = '1.0b1.dev0'
 description = '.gov.br: Tradução de Português para Libras'
 long_description = (
     open('README.rst').read() + '\n' +
